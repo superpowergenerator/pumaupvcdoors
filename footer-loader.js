5280047195f91a21bde5f2c5325dcf8ed35483ae
superpowergenerator <digizonne17@gmail.com>
2026-06-24 02:19:53 -0700

    Update footer-loader.js

--- footer-loader.js
+++ footer-loader.js
@@ -1,5 +1,9 @@
-fetch("footer.html")
-  .then(res => res.text())
+fetch("/footer.html")
+  .then(res => {
+    if (!res.ok) throw new Error("Footer not found");
+    return res.text();
+  })
   .then(html => {
     document.getElementById("site-footer").innerHTML = html;
-  });
+  })
+  .catch(err => console.error(err));
